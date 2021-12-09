b6859eb263b999cf65ead2428f00d8564a52004f
Pawel Sendyk <pawelsendyk@berkeley.edu>
2021-12-09 14:10:29 -0700

    Revert "more logging"
    This reverts commit c1aefebce9c2c98da7e7be54b13a0656881af587.

--- src/actions/heap/heap.ts
+++ src/actions/heap/heap.ts
@@ -113,7 +113,6 @@ export class HeapAction extends Hub.Action {
       onRow: (row) => {
         try {
           const { heapFieldValue, properties } = this.extractPropertiesFromRow(
-            request.params.heap_env_id!,
             row,
             heapFieldName,
             heapFieldLabel,
@@ -255,7 +254,6 @@ export class HeapAction extends Hub.Action {
   }
 
   private extractPropertiesFromRow(
-    appId: string,
     row: Hub.JsonDetail.Row,
     heapFieldName: string,
     heapFieldLabel: string,
@@ -274,9 +272,6 @@ export class HeapAction extends Hub.Action {
 
     const properties: { [K in string]: string } = {}
     for (const [fieldName, cell] of Object.entries(row)) {
-      if (appId === HeapAction.HEAP_ENV_ID) {
-        winston.info("HoH field", fieldName, cell)
-      }
       if (fieldName !== heapFieldName) {
         const field = allFieldMap[fieldName]
         // Field labels are the original name of the property that has not been sanitized or snake-cased.
